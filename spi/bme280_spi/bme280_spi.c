a403b1387860621016c4eb91bb80f842728c29e7
Juergen Heeks <jurgen.heeks@gmail.com>
2021-04-17 21:42:35 +0200

    Let SPI HW control the chip select
    * configure chip select pin as GPIO_FUNC_SPI
    * use mode CPOL=1, CPHA=1
    * do a read_registers by one call to spi_write_read_blocking()

--- spi/bme280_spi/bme280_spi.c
+++ spi/bme280_spi/bme280_spi.c
@@ -105,49 +105,32 @@ uint32_t compensate_humidity(int32_t adc_H) {
     return (uint32_t) (v_x1_u32r >> 12);
 }
 
-#ifdef PICO_DEFAULT_SPI_CSN_PIN
-static inline void cs_select() {
-    asm volatile("nop \n nop \n nop");
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 0);  // Active low
-    asm volatile("nop \n nop \n nop");
-}
-
-static inline void cs_deselect() {
-    asm volatile("nop \n nop \n nop");
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
-    asm volatile("nop \n nop \n nop");
-}
-#endif
-
 #if defined(spi_default) && defined(PICO_DEFAULT_SPI_CSN_PIN)
 static void write_register(uint8_t reg, uint8_t data) {
     uint8_t buf[2];
     buf[0] = reg & 0x7f;  // remove read bit as this is a write
     buf[1] = data;
-    cs_select();
     spi_write_blocking(spi_default, buf, 2);
-    cs_deselect();
-    sleep_ms(10);
 }
 
-static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
+static uint8_t rxBuf[30];
+static uint8_t txBuf[30];
+
+static uint8_t* read_registers(uint8_t reg, uint16_t len) {
     // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
+    // first, and subsequently read from the device. The register is auto incrementing
     // so we don't need to keep sending the register we want, just the first.
-    reg |= READ_BIT;
-    cs_select();
-    spi_write_blocking(spi_default, &reg, 1);
-    sleep_ms(10);
-    spi_read_blocking(spi_default, 0, buf, len);
-    cs_deselect();
-    sleep_ms(10);
+    // This is kept together in one spi call to get the chip select controlled correctly.
+    txBuf[0] = reg | READ_BIT;
+    spi_write_read_blocking(spi_default, txBuf, rxBuf, len+1);
+    return &rxBuf[1];
 }
 
 /* This function reads the manufacturing assigned compensation parameters from the device */
 void read_compensation_parameters() {
-    uint8_t buffer[26];
+    uint8_t* buffer;
 
-    read_registers(0x88, buffer, 24);
+    buffer = read_registers(0x88, 24);
 
     dig_T1 = buffer[0] | (buffer[1] << 8);
     dig_T2 = buffer[2] | (buffer[3] << 8);
@@ -165,7 +148,7 @@ void read_compensation_parameters() {
 
     dig_H1 = buffer[25];
 
-    read_registers(0xE1, buffer, 8);
+    buffer = read_registers(0xE1, 8);
 
     dig_H2 = buffer[0] | (buffer[1] << 8);
     dig_H3 = (int8_t) buffer[2];
@@ -175,9 +158,9 @@ void read_compensation_parameters() {
 }
 
 static void bme280_read_raw(int32_t *humidity, int32_t *pressure, int32_t *temperature) {
-    uint8_t buffer[8];
+    uint8_t* buffer;
 
-    read_registers(0xF7, buffer, 8);
+    buffer = read_registers(0xF7, 8);
     *pressure = ((uint32_t) buffer[0] << 12) | ((uint32_t) buffer[1] << 4) | (buffer[2] >> 4);
     *temperature = ((uint32_t) buffer[3] << 12) | ((uint32_t) buffer[4] << 4) | (buffer[5] >> 4);
     *humidity = (uint32_t) buffer[6] << 8 | buffer[7];
@@ -195,23 +178,19 @@ int main() {
 
     // This example will use SPI0 at 0.5MHz.
     spi_init(spi_default, 500 * 1000);
+    // chip select is handled by SPI HW block in case of CPOL=1, CPHA=1
+    spi_set_format(spi_default, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI);
     // Make the SPI pins available to picotool
-    bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
-
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
-    gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
-    // Make the CS pin available to picotool
-    bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
+    bi_decl(bi_4pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI));
 
     // See if SPI is working - interrograte the device for its I2C ID number, should be 0x60
-    uint8_t id;
-    read_registers(0xD0, &id, 1);
-    printf("Chip ID is 0x%x\n", id);
+    uint8_t* buffer_id;
+    buffer_id = read_registers(0xD0, 1);
+    printf("Chip ID is 0x%x\n", buffer_id[0]);
 
     read_compensation_parameters();
 
